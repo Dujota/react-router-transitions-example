80d103829287ee8fe266adbe6b22c6e7bcc3316e
Denis <ddujota@internationalsafety.com>
2019-01-27 22:10:18 -0500

    formatted js files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import {
-  Route,
-  NavLink,
-  Switch
-} from "react-router-dom";
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import About from './About';
 
 // class component
 class App extends Component {
-
   render() {
-
     return (
-      <div className="App">
-        <div className="nav">
-          <NavLink exact to="/" activeClassName="active">Home</NavLink>
-          <NavLink to="/about" activeClassName="active">About</NavLink>
+      <div className='App'>
+        <div className='nav'>
+          <NavLink exact to='/' activeClassName='active'>
+            Home
+          </NavLink>
+          <NavLink to='/about' activeClassName='active'>
+            About
+          </NavLink>
         </div>
+
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
         </Switch>
       </div>
     );
