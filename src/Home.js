829c9cc6ce8f1fed3620aeee06f634d18a981f77
Denis <ddujota@internationalsafety.com>
2019-01-27 22:10:28 -0500

    formattede all files

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,20 @@ import React from 'react';
 import logo from './assets/logo.svg';
 
 const Home = () => {
-    return(
-        <div className="page">
-            <section>
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>How to create animated page transitions in React?</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias laboriosam maiores autem nemo consectetur sed perspiciatis minus quibusdam dolorem ratione sapiente amet quia quis voluptates, earum culpa odio.</p>
-            </section>
-        </div>   
-    )
-}
+  return (
+    <div className='page'>
+      <section>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>How to create animated page transitions in React?</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+          laboriosam maiores autem nemo consectetur sed perspiciatis minus
+          quibusdam dolorem ratione sapiente amet quia quis voluptates, earum
+          culpa odio.
+        </p>
+      </section>
+    </div>
+  );
+};
 
 export default Home;
